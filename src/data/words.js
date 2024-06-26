05a5c94c7bf5baa46b6cbd4b0f500769eb72f0a8
Marcos Guimaraes <marcosvg10@hotmail.com>
2024-06-26 11:46:19 -0300

    Adicionado tela inicial e componentes

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList ={
+    carro: ["Motor", "Porta", "Pneu", "Antena", "Volante"],
+    fruta: ["Banana","Laranja","Abacate", "Abacaxi","Acerola"],
+    corpo: ["Perna","Olhos","Orelha","Joelho","Unha"],
+    computador: ["Mouse","Teclado","Monitor","Gabinete","Fonte"],
+    progamação: ["JavaScript","Python","React","Java","Delphi"],
+    alimento: ["Arroz","Ovo","Carne","Leite","Tomate"]
+};
